87460ef546c9cea8a778ed1099113ba32bc4f3ec
makhk <harikrish2050@gmail.com>
2021-06-02 12:15:37 +0530

    X00T: Add missing libs for WFD
    Signed-off-by: NKTN30 <nk.whitehat@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/nktn30/android_packages_apps_GCamGOPrebuilt -b eleven packages/apps/GCamGOPrebuilt
-rm -rf vendor/qcom/opensource/power 
+rm -rf vendor/qcom/opensource/power
 git clone https://github.com/nktn30/vendor_qcom_opensource_power -b eleven vendor/qcom/opensource/power
+
+rm -rf hardware/qcom-caf/msm8998/display
+git clone -b 11 https://github.com/nktn30/hardware_qcom-caf_display_msm8998.git hardware/qcom-caf/msm8998/display
