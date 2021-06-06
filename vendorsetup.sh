3d6244d66337d1d1ed045b9d321f5e07f702d523
NKTN30 <nkwhitehat@gmail.com>
2021-06-06 16:58:52 +0530

    X00T : Track op-launcher
    Signed-off-by: NKTN30 <nkwhitehat@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/NKTN30/android_packages_apps_GCamGOPrebuilt -b eleven packages/apps/GCamGOPrebuilt
+git clone https://github.com/nktn30/android_packages_apps_GCamGOPrebuilt -b eleven packages/apps/GCamGOPrebuilt
 rm -rf vendor/qcom/opensource/power 
-git clone https://github.com/NKTN30/vendor_qcom_opensource_power -b eleven vendor/qcom/opensource/power
+git clone https://github.com/nktn30/vendor_qcom_opensource_power -b eleven vendor/qcom/opensource/power
+git clone https://github.com/nktn30/android_vendor_oplauncher vendor/oplauncher
